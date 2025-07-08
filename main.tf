24b90048c90f5a2d6e030e47a9f512ef8ec0c325
leoeric <leoeric13@gmail.com>
2025-07-08 21:46:25 +0800

    3rd comment

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "my-terraform-state-bucket"   # Replace with your bucket name
     key    = "envs/dev/terraform.tfstate"  # Unique file path in the bucket
-    region = "ap-southeast-1"              # Must match the bucket's region
+    region = "us-west-2"              # Must match the bucket's region
   }
 }
